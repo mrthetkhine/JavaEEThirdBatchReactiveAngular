734419979cbbb6fd514c5c262f94fab16f0e8c9e
Thet Khine <mrthetkhine@gmail.com>
2021-08-14 19:07:14 +0630

    Add Scheduler

--- SpringReactiveAngular/src/main/java/com/javaeethirdbatch/SpringReactiveAngularApplication.java
+++ SpringReactiveAngular/src/main/java/com/javaeethirdbatch/SpringReactiveAngularApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+//@EnableScheduling
 @SpringBootApplication
-//@EnableMongoRepositories
-//@EnableReactiveMongoRepositories
+@EnableMongoRepositories
+@EnableReactiveMongoRepositories
 public class SpringReactiveAngularApplication {
 
 	public static void main(String[] args) {
